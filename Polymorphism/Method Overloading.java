de48de4b6832dc4f8cc59f5a4e212342d90c2aad
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2022-01-21 22:18:39 +0530

    Update Method Overloading.java

--- Polymorphism/Method Overloading.java
+++ Polymorphism/Method Overloading.java
@@ -1,1 +1,39 @@
+Method Overloading-Changing no. of arguments:
+-------------------------------------------------------
+class Adder{  
+static int add(int a,int b){return a+b;}  
+static int add(int a,int b,int c){return a+b+c;}  
+}  
+class TestOverloading1{  
+public static void main(String[] args){  
+System.out.println(Adder.add(11,11));  
+System.out.println(Adder.add(11,11,11));  
+}}  
+
+Method Overloading-Changing data type of arguments:
+--------------------------------------------------------
+class Adder{  
+static int add(int a, int b){return a+b;}  
+static double add(double a, double b){return a+b;}  
+}  
+class TestOverloading2{  
+public static void main(String[] args){  
+System.out.println(Adder.add(11,11));  
+System.out.println(Adder.add(12.3,12.6));  
+}}  
+
+Method Overloading-Changing the sequence of arguments:
+----------------------------------------------------------
+class Adder{  
+static int add(int a, float b){return a+b;}  
+static double add(float a, int b){return a+b;}  
+}  
+class TestOverloading2{  
+public static void main(String[] args){  
+System.out.println(Adder.add(11,12.6));  
+System.out.println(Adder.add(12.6,11));  
+}}  
+
+Method Overloading-Changing the return type of method:Is not possible
+-------------------------------------------------------------------------------------
 
